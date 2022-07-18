a0d4e4c993b594c1aa0ecc292e831767cc6004e5
rafaelmasselli <rafaelmasselli0@gmail.com>
2022-07-18 19:43:20 -0300

    Feat: iginit react-native

---
+++ App.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+import { NativeBaseProvider, StatusBar } from "native-base";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+
+import { SignIn } from "./src/screens/SignIn";
+import { Loading } from "./src/components/Loading";
+
+import { THEME } from "./src/styles/theme";
+
+export default function App() {
+  const [fontsLoading] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
+  return (
+    <NativeBaseProvider theme={THEME}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoading ? <SignIn /> : <Loading />}
+    </NativeBaseProvider>
+  );
+}
