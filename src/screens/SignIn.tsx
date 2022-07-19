a98d32eff5f5a18a86b83eead5952386e652e18d
rafaelmasselli <rafaelmasselli0@gmail.com>
2022-07-18 23:15:19 -0300

    feat: aula 1

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Heading, VStack, Icon, useTheme } from "native-base";
 import { Envelope, Key } from "phosphor-react-native";
 
@@ -7,6 +8,9 @@ import { Button } from "../components/Button";
 import Logo from "./../assets/logo_primary.svg";
 
 export function SignIn() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   const { colors } = useTheme();
 
   return (
@@ -22,14 +26,17 @@ export function SignIn() {
         InputLeftElement={
           <Icon as={<Envelope color={colors.gray[300]} />} ml={4} />
         }
+        onChangeText={setEmail}
       />
       <Input
+        mb={8}
         placeholder="Senha"
         InputLeftElement={<Icon as={<Key color={colors.gray[300]} />} ml={4} />}
         secureTextEntry
+        onChangeText={setPassword}
       />
 
-      <Button title="Entrar" w="full"/>
+      <Button title="Entrar" w="full" />
     </VStack>
   );
 }
